7cae784b21897a406709b5e1a44e22bd349500b0
termimi <123395118+termimi@users.noreply.github.com>
2024-09-27 13:58:49 +0200

    Chore: Ajout des données des équipe
    Ajout d'un fichier CSV contenant toutes les donnée des équipe

--- App/NBAStatsCalculator/obj/Debug/net8.0-windows/NBAStatsCalculator.AssemblyInfo.cs
+++ App/NBAStatsCalculator/obj/Debug/net8.0-windows/NBAStatsCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NBAStatsCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da33a9f4f7bec292fc5c536794b763a9ef31442a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a988e439b2e410eb1b9f14c5ddbfbe06fae5a60e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NBAStatsCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NBAStatsCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
