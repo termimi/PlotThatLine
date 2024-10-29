4045c787e1a7aabbec88a63f1eac600f3a45d04a
termimi <123395118+termimi@users.noreply.github.com>
2024-10-29 19:35:54 +0100

    Chore: (DataSelection.cs) Ajout d'un fichier de test pour la classe DataSelection

---
+++ App/NBAStatsCalculatorTests/DataSelectionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NBAStatsCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBAStatsCalculator.Tests
+{
+    [TestClass()]
+    public class DataSelectionTests
+    {
+        [TestMethod()]
+        public void loadFileTest()
+        {
+
+            Assert.Fail();
+        }
+    }
+}
